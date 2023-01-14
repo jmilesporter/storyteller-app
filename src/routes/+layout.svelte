2152c7881b3f1ede85ecb0cf043c656af8989d58
Jeffrey Porter <jmilesporter@gmail.com>
2023-01-14 15:52:14 -0500

    initial commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,22 @@
 	import Overlay from '../components/Overlay.svelte';
 </script>
 
-<head
-	><script src="https://kit.fontawesome.com/21639931c9.js" crossorigin="anonymous"></script></head
->
+<head>
+	<script src="https://kit.fontawesome.com/21639931c9.js" crossorigin="anonymous"></script>
 
-{#if $isOverlayOpen}
+	<!-- <script
+		type="text/javascript"
+		src="../../node_modules/fullpage.js/dist/fullpage.extensions.min.js"
+	></script> -->
+</head>
+
+<!-- {#if $isOverlayOpen}
 	<div>
 		<Overlay>
 			<div />
 		</Overlay>
 	</div>
-{/if}
+{/if} -->
 <main>
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,60 @@
 	import { isOverlayOpen } from '../stores/OverlayStore.js';
 	import { lyrics } from '../stores/OverlayStore.js';
 	import { loading } from '../stores/OverlayStore.js';
+	import { onMount, onDestroy } from 'svelte';
+	import fullpage from 'fullpage.js/dist/fullpage.extensions.min.js';
+
+	let myFullpage;
+
+	function destroyFullPage() {
+		if (myFullpage) {
+			myFullpage.destroy('all');
+			myFullpage = null;
+		}
+	}
+
+	function createFullPage() {
+		if (myFullpage) {
+			return;
+		}
+
+		const check = document.getElementById('fullpage');
+		console.log(check);
+		if (document.getElementById('fullpage')) {
+			console.log('fullpage exists');
+			myFullpage = new fullpage('#fullpage', {
+				// fullpage.js options here
+				licenseKey: '1L9FI-Q6NI9-RIWI7-V1KFJ-TLBYL',
+				controlArrows: false,
+				scrollHorizontally: true,
+				slideNavigation: true,
+				autoScrolling: true,
+				continuousVertical: true,
+				scrollingSpeed: 500,
+				// parallax: true,
+				// anchors: ['firstPage', 'secondPage', 'thirdPage'],
+				afterLoad: function (origin, destination, direction, trigger) {
+					console.warn('afterLoad. Trigger: ' + trigger);
+				}
+			});
+			console.log('myFullpage: ', myFullpage);
+		}
+	}
+
+	export let active = false;
+
+	$: {
+		if (active) createFullPage();
+		else destroyFullPage();
+	}
+
+	onMount(() => {
+		createFullPage();
+	});
+
+	onDestroy(() => {
+		destroyFullPage();
+	});
 
 	export let data;
 	const apiKey = data.key;
@@ -45,84 +99,104 @@
 	$: artist = '';
 	$: structure = '';
 
-	switch (structure) {
-		case 'verse-verse-verse':
-			structure = 'verse-verse-verse';
-			break;
-		case 'verse-chorus':
-			structure = 'verse-chorus-verse-chorus-verse-chorus';
-			break;
-		case 'verse-bridge':
-			structure = 'verse-verse-bridge-verse-bridge-verse';
-			break;
-		case 'verse-prechorus-chorus':
-			structure = 'verse-prechorus-chorus-verse-prechorus-chorus';
-			break;
-		default:
-			structure = '';
-	}
-
 	$: lyricPrompt = `${genre} lyrics  on the subject of ${subject} in the style of ${artist}, ${structure}`;
 </script>
 
-<div class="grid place-items-center bg-slate-500 h-screen ">
-	<input
-		placeholder="s u b j e c t"
-		class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={subject}
-	/>
-	<select
-		class="absolute right-0 text-center bg-slate-700  w-72 p-8 shadow-xl rounded-3xl text-gray-200 hover:-translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={genre}
-	>
-		<option class="rotate-90" value="" disabled selected>g e n r e</option>
+<div id="fullpage">
+	<div class="section " id="section 1">
+		<div class="slide " id="slide1">
+			<div class="fp-bg " />
 
-		<option>rap</option>
-		<option>rock</option>
-		<option>pop</option>
-		<option>folk</option>
-		<option>country</option>
-		<option>blues</option>
-		<option>r&b</option>
-		<option>metal</option>
-		<option>classical</option>
-		<option>reggae</option>
-		<option>electronic</option>
-		<option>indie</option>
-		<option>punk</option>
-		<option>disco</option>
-		<option>house</option>
-		<option>techno</option>
-	</select>
-	<input
-		placeholder="a r t i s t"
-		class="absolute bottom-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700  p-8 w-72 shadow-xl rounded-3xl text-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={artist}
-	/>
-	<select
-		class="absolute absolute left-0 text-center bg-slate-700  w-72 p-8 shadow-xl rounded-3xl text-gray-200 hover:translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={structure}
-	>
-		<option value="" disabled selected>s t r u c t u r e</option>
-		<option>verse-verse-verse</option>
-		<option>verse-chorus</option>
-		<option>verse-bridge</option>
-		<option>verse-chorus-bridge</option>
-		<option>verse-prechorus-chorus</option>
-	</select>
-
-	<button
-		id="generate"
-		class="bg-slate-900  p-8 w-72 shadow-xl rounded-3xl text-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-slate-800 hover: transition ease-in-out duration-500 delay-300  "
-		on:click={() => {
-			onClick();
-		}}>g e n e r a t e &nbsp; l y r i c s</button
-	>
+			<input
+				placeholder="s u b j e c t (s)"
+				class=" placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+				bind:value={subject}
+			/>
+		</div>
+		<div class="slide " id="slide2">
+			<div class="fp-bg " />
+			<input
+				placeholder="a r t i s t (s)"
+				class=" placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+				bind:value={artist}
+			/>
+		</div>
+		<div class="slide" id="slide3">
+			<div class="fp-bg  " />
+			<input
+				placeholder="g e n r e (s)"
+				class=" placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+				bind:value={genre}
+			/>
+		</div>
+	</div>
+	<div class="section place-items-center bg-gray-200 " id="section 4">
+		<button
+			id="generate"
+			class="bg-slate-900 p-8 w-72 shadow-xl rounded-3xl text-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-slate-800 hover: transition ease-in-out duration-500 delay-300  "
+			on:click={() => {
+				onClick();
+			}}>g e n e r a t e &nbsp; l y r i c s</button
+		>
+	</div>
 </div>
 
-<style>
+<!-- <style>
 	select {
 		appearance: none;
 		cursor: pointer;
+	}
+</style> -->
+
+<!-- 
+	- tooltips or notes giving a hint to the user either an explanation or examples
+
+
+	- subject
+	- genre
+	- artist
+	- title
+
+	- temperature 
+	- max tokens ('haikus', 'poetry', 'single line', 'single section', 'single stanza', 'single song', 'full EP', 'full album'')
+
+	- various precon settings as buttons (can help teach how to use the app) (auto populates fields and parameters)
+	- it needs to remember your prompts and its responses to feed back in like normal chat gpt does 
+	- a chat window that you can type in and it will respond to you 'i like this line, build a new set of lyrics aound it', etc
+
+	- structure 
+	- rhyme scheme
+	- rhyme type
+	- line length
+	- line number 
+
+	- 'write a song using these lyrics'
+	- finish this stanza 
+	- finish this verse
+	- give me a line  
+
+
+	- feedback or rating system
+	- The GPT Top 100 
+	
+-->
+<style>
+	#slide1 .fp-bg {
+		background-size: cover;
+		background-position: initial;
+		background-image: url(slide1.jpeg);
+		height: 100vh;
+	}
+	#slide2 .fp-bg {
+		background-size: cover;
+		background-position: center 80%;
+		background-image: url(slide2.jpeg);
+		height: 100vh;
+	}
+	#slide3 .fp-bg {
+		background-size: cover;
+		background-position: center 80%;
+		background-image: url(slide3.jpeg);
+		height: 100vh;
 	}
 </style>
